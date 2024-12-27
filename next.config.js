61985c6d64b9cde60b83738f999049b74e0f5756
DonPQ <107664839+DonPQ@users.noreply.github.com>
2024-12-27 19:01:27 +0700

    Ignore build

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@
  */
 const nextConfig = {
   output: "export",
-  ignoreDuringBuilds: true,
+  eslint: {
+    ignoreDuringBuilds: ['/src']
+  }
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
 
